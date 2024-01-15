aa4349ba2dd47dc1499e01f81c63a65583650f0d
Elle Crawshaw <ellecrawshaw@Elles-MacBook-Air.local>
2024-01-15 15:59:37 +0000

    GET /api/articles/:article_id

--- data_handling/app.js
+++ data_handling/app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
 
-const { getTopics , getEndpoints } = require('./controllers/topics.controllers');
+const { getTopics , getEndpoints , getArticles } = require('./controllers/topics.controllers');
 
 app.get('/api', getEndpoints);
 
 app.get('/api/topics', getTopics);
 
+app.get('/api/articles', getArticles);
+app.get('/api/articles/:id', getArticles);
+
+
+app.use((err ,req, res, next) => {
+    if (err.msg === "Not Found") {
+      res.status(404).send({msg: 'Not Found'})
+    }else{
+      next(err);
+    }
+  });
+  app.use((err, req, res, next) => {
+    if (err.code ==='42703') {
+        res.status(400).send({msg: 'Bad Request'})
+    }else{
+        next(err);
+    }
+  })
 
 
 module.exports = app;
